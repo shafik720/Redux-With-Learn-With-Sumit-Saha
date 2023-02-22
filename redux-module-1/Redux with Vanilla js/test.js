a81083bb5a17f952fd797b503fc4ee835dcebd98
shafik720 <shafikrasel5@gmail.com>
2023-02-22 21:43:05 +0600

    added new folder

--- redux-module-1/Redux with Vanilla js/test.js
+++ redux-module-1/Redux with Vanilla js/test.js
@@ -1,5 +1,81 @@
-const getMax = (a, b) => Math.max(a, b);
+// Define the initial state
+const initialState = {
+    counters: [0, 0, 0,0,0],
+  };
+  
+  // Define the actions
+  const INCREMENT = 'INCREMENT';
+  const DECREMENT = 'DECREMENT';
+  
+  // Define the action creators
+  const incrementCounter = (index) => ({
+    type: INCREMENT,
+    index,
+  });
+  
+  const decrementCounter = (index) => ({
+    type: DECREMENT,
+    index,
+  });
+  
+  // Define the reducer
+  const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case INCREMENT:
+        return {
+          ...state,
+          counters: state.counters.map((counter, index) =>
+            index === action.index ? counter + 5 : counter
+          ),
+        };
+      case DECREMENT:
+        return {
+          ...state,
+          counters: state.counters.map((counter, index) =>
+            index === action.index ? counter - 5 : counter
+          ),
+        };
+      default:
+        return state;
+    }
+  };
+  
+  // Create the store
+  const store = Redux.createStore(reducer);
+  
+  // Update the UI whenever the state changes
+  const updateUI = () => {
+    const counters = document.querySelectorAll('.counter');
+    counters.forEach((counter, index) => {
+      counter.innerText = store.getState().counters[index];
+    });
+  };
+  
+  store.subscribe(updateUI);
+  updateUI();
+  
+  // Handle button clicks
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      if (button.classList.contains('increment-btn')) {
+        store.dispatch(incrementCounter(index));
+      } else if (button.classList.contains('decrement-btn')) {
+        store.dispatch(decrementCounter(index));
+      }
+    });
+  });
+  
 
-const result = [1, 5, 7, 10, 0, 22, 37].reduce(getMax);
 
-console.log(result);
+//   ------------------
+
+
+
+const counters = document.querySelectorAll('.counter');
+    counters.forEach((counter, index) => {
+    //   counter.innerText = store.getState().counters[index];
+    // console.log(counter, index);
+
+    // counter.innerText = 5
+    });
--- redux-module-1/Redux with Vanilla js/test2.js
+++ redux-module-1/Redux with Vanilla js/test2.js
@@ -43,41 +43,29 @@ const initialState = {
   
   // Define counter component
   function Counter({ value, onIncrement, onDecrement }) {
-    const counterElement = document.createElement('div');
-    counterElement.className = 'counter';
-  
-    const decrementButton = document.createElement('button');
-    decrementButton.className = 'decrement';
-    decrementButton.innerHTML = '-';
-    decrementButton.addEventListener('click', onDecrement);
-    counterElement.appendChild(decrementButton);
-  
-    const valueElement = document.createElement('span');
-    valueElement.className = 'value';
-    valueElement.innerHTML = value;
-    counterElement.appendChild(valueElement);
-  
-    const incrementButton = document.createElement('button');
-    incrementButton.className = 'increment';
-    incrementButton.innerHTML = '+';
-    incrementButton.addEventListener('click', onIncrement);
-    counterElement.appendChild(incrementButton);
-  
-    return counterElement;
+    return (
+      <div class="counter">
+        <button class="decrement" onClick={onDecrement}>-</button>
+        <span class="value">{value}</span>
+        <button class="increment" onClick={onIncrement}>+</button>
+      </div>
+    );
   }
   
   // Define counters component
   function Counters({ counters }) {
-    const countersElement = document.createElement('div');
-    counters.forEach((value, index) => {
-      const counter = Counter({
-        value,
-        onIncrement: () => store.dispatch(incrementCounter(index)),
-        onDecrement: () => store.dispatch(decrementCounter(index))
-      });
-      countersElement.appendChild(counter);
-    });
-    return countersElement;
+    return (
+      <div>
+        {counters.map((value, index) =>
+          <Counter
+            key={index}
+            value={value}
+            onIncrement={() => store.dispatch(incrementCounter(index))}
+            onDecrement={() => store.dispatch(decrementCounter(index))}
+          />
+        )}
+      </div>
+    );
   }
   
   // Render app
@@ -85,8 +73,7 @@ const initialState = {
     const state = store.getState();
     const countersElement = document.getElementById('counters');
     countersElement.innerHTML = '';
-    const counters = Counters({ counters: state.counters });
-    countersElement.appendChild(counters);
+    ReactDOM.render(<Counters counters={state.counters} />, countersElement);
   }
   
   store.subscribe(render);
