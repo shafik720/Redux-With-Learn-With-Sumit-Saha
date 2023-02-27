c8515e8c32425dd799721da183f55c0e16d32bc2
shafik720 <shafikrasel5@gmail.com>
2023-02-27 14:09:23 +0600

    added changes

--- redux-module-3/To-do List/src/App.js
+++ redux-module-3/To-do List/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
+import TodoList from './Components/TodoList/TodoList';
 
 function App() {
   return (
     <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
       <Navbar></Navbar>
       <Header></Header>
+      <TodoList></TodoList>
     </div>
   );
 }
