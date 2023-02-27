504577c066a01ac2af2050b0d43ccc219c1e1118
shafik720 <shafikrasel5@gmail.com>
2023-02-27 21:41:06 +0600

    action creators created

---
+++ redux-module-3/To-do List/src/redux/todos/actionCreators.js
@@ -0,0 +1,45 @@
+
+import {ADDED, ALLCOMPLETE, CLEARCOMPLETE, COLORSELECTED, DELETED, TOGGOLED} from './actionIdentifiers'
+
+export const addTask = (todoText) => {
+    return{
+        type : ADDED,
+        payload : todoText
+    }
+}
+
+export const completeAll = () => {
+    return{
+        type : ALLCOMPLETE
+    }
+}
+
+export const toggle = (toggleId) => {
+    return {
+        type : TOGGOLED,
+        payload : toggleId,
+    }
+}
+
+export const colorSelection = (id, color) => {
+    return {
+        type : COLORSELECTED,
+        payload : {
+            elementId : id,
+            colors : color
+        }
+    }
+}
+
+export const clearCompleted = () => {
+    return{
+        type : CLEARCOMPLETE
+    }
+}
+
+export const deleteSingle = (id) => {
+    return {
+        type : DELETED,
+        payload : id
+    }
+}
---
+++ redux-module-3/To-do List/src/redux/todos/actionIdentifiers.js
@@ -0,0 +1,9 @@
+
+const ADDED = "todos/added" ; 
+const ALLCOMPLETE = "todos/allComplete " ; 
+const CLEARCOMPLETE = "todos/clear" ; 
+const DELETED = "todos/delete" ; 
+const TOGGOLED = "todos/toggle" ; 
+const COLORSELECTED = "todos/colors" ; 
+
+export {ADDED, ALLCOMPLETE, CLEARCOMPLETE, COLORSELECTED, DELETED, TOGGOLED}
