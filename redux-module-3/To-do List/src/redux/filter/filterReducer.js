3b01a8d58667d2dac20734949e212d4822f1fc54
shafik720 <shafikrasel5@gmail.com>
2023-02-28 13:25:39 +0600

    filter  reducer created

---
+++ redux-module-3/To-do List/src/redux/filter/filterReducer.js
@@ -0,0 +1,35 @@
+import { COLORSELECTED, STATUSCHANGED } from "./actionIdentify";
+import { initialState } from "./initialState";
+
+
+export const filterReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case STATUSCHANGED:
+            return {
+                ...state,
+                status : action.payload
+            }
+    
+        case COLORSELECTED :
+            const {color, changeTypes} = action.payload; 
+                switch (changeTypes) {
+                    case 'added':
+                        return {
+                            ...state,
+                            colors : [
+                                ...state.colors,
+                                color
+                            ]
+                        }
+                
+                    case 'removed' : 
+                        return {
+                            ...state,
+                            colors : state.colors.filter(existingColor => existingColor !== color)
+                        }
+                    default:
+                        return state;
+                }
+        default:
+            return state;
+}
---
+++ redux-module-3/To-do List/src/redux/filter/initialState.js
@@ -0,0 +1,6 @@
+
+
+export const initialState = {
+    states : 'all',
+    colors : []
+}
