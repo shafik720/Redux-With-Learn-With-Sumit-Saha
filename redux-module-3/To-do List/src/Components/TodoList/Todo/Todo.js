03ab1988da1242efc11cb1753e2e3b3843c89513
shafik720 <shafikrasel5@gmail.com>
2023-02-28 14:46:37 +0600

    todo state initialized

--- redux-module-3/To-do List/src/Components/TodoList/Todo/Todo.js
+++ redux-module-3/To-do List/src/Components/TodoList/Todo/Todo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import cancelImg from '../../../assets/images/cancel.png'
 
-const Todo = ({todo}) => {
-    const {id, completed, text} = todo;
+const Todo = ({ todo }) => {
+    const { id, completed, text, color } = todo;
     return (
         <div>
             <div
@@ -10,14 +10,14 @@ const Todo = ({todo}) => {
                 <div
                     className="rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 border-green-500 focus-within:border-green-500">
                     <input type="checkbox" className="opacity-0 absolute rounded-full" />
-                    <svg className="hidden fill-current w-3 h-3 text-green-500 pointer-events-none" viewBox="0 0 20 20">
+                    <svg className={`fill-current w-3 h-3 text-green-500 pointer-events-none ${!completed && 'hidden'}`} viewBox="0 0 20 20">
                         <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
                     </svg>
                 </div>
 
                 <div className="select-none flex-1">
                     {text}
-                    </div>
+                </div>
 
                 <div
                     className="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-green-500 hover:bg-green-500 bg-green-500">
