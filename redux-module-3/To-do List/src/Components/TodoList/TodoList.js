2c9a4d842d794beaa5f3977df1884abdad404f13
shafik720 <shafikrasel5@gmail.com>
2023-02-28 20:10:43 +0600

    color filtering done

--- redux-module-3/To-do List/src/Components/TodoList/TodoList.js
+++ redux-module-3/To-do List/src/Components/TodoList/TodoList.js
@@ -5,27 +5,38 @@ import Todo from './Todo/Todo';
 const TodoList = () => {
     const todos = useSelector((state) => state.todos);
     const filters = useSelector((state) => state.filter);
-    return (
-            <div className="">
-                {
-                    todos.filter(todo => {
-                        const {status} = filters;
-                        console.log(todo);
-                        switch (status) {
-                            case 'complete':
-                                return todo.completed;
 
-                            case 'incomplete' : 
-                                return !todo.completed;
-                            default:
-                                return true;
-                        }
-                    }).map(todo => <Todo
-                        todo = {todo}
-                        key = {todo.id}
-                    ></Todo>)
-                }
-            </div>
+    const filterByStatus = todo => {
+        const { status } = filters;
+        switch (status) {
+            case 'complete':
+                return todo.completed;
+
+            case 'incomplete':
+                return !todo.completed;
+            default:
+                return true;
+        }
+    }
+
+    const filterByColors = todo => {
+        const { colors } = filters;
+        console.log(todo);
+        if (colors.length > 0) {
+            return colors.includes(todo?.color)
+        }else{
+            return true;
+        }
+    }
+    return (
+        <div className="">
+            {
+                todos.filter(filterByStatus).filter(filterByColors).map(todo => <Todo
+                    todo={todo}
+                    key={todo.id}
+                ></Todo>)
+            }
+        </div>
     );
 };
 
