cb9af2100b8987cc3cf7da403eddd66147df7960
shafik720 <shafikrasel5@gmail.com>
2023-02-28 19:58:47 +0600

    completetion filters done

--- redux-module-3/To-do List/src/Components/TodoList/TodoList.js
+++ redux-module-3/To-do List/src/Components/TodoList/TodoList.js
@@ -4,10 +4,23 @@ import Todo from './Todo/Todo';
 
 const TodoList = () => {
     const todos = useSelector((state) => state.todos);
+    const filters = useSelector((state) => state.filter);
     return (
             <div className="">
                 {
-                    todos.map(todo => <Todo
+                    todos.filter(todo => {
+                        const {status} = filters;
+                        console.log(todo);
+                        switch (status) {
+                            case 'complete':
+                                return todo.completed;
+
+                            case 'incomplete' : 
+                                return !todo.completed;
+                            default:
+                                return true;
+                        }
+                    }).map(todo => <Todo
                         todo = {todo}
                         key = {todo.id}
                     ></Todo>)
