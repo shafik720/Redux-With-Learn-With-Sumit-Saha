785a28129e0a3185af5b36d6c2938b5240591174
shafik720 <shafikrasel5@gmail.com>
2023-03-04 14:39:40 +0600

    rtk initiated

--- redux-module-5/think-in-a-redux-way-5.2/rtk/features/counter/counterSlice.js
+++ redux-module-5/think-in-a-redux-way-5.2/rtk/features/counter/counterSlice.js
@@ -1,25 +1,25 @@
-
-// const counterReducer = require('@reduxjs/toolkit');
-
 const { createSlice } = require("@reduxjs/toolkit")
 
+// initial state
 const initialState = {
     count : 0
 }
 
 const counterSlice = createSlice({
-    name : 'counter',
+    name : "counter",
     initialState,
     reducers : {
         increment : (state, action) => {
             state.count++
         },
-        decrement : (state, action) => {
-            state.count--
+        decrement : {
+            decrement : (state, action) => {
+                state.count--
+            }
         }
-    }
+    }    
 })
 
 
-modules.exports = counterSlice.reducer;
-modules.exports.counterActions = counterSlice.actions;
+module.exports = counterSlice.reducer;
+module.exports.counterActions = counterSlice.actions
