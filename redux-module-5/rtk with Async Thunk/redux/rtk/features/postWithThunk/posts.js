ef38b04765480f33c7628c24978441e52a2a3e87
shafik720 <shafikrasel5@gmail.com>
2023-03-05 13:03:35 +0600

    redux toolkit with thunk

---
+++ redux-module-5/rtk with Async Thunk/redux/rtk/features/postWithThunk/posts.js
@@ -0,0 +1,40 @@
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit")
+
+
+const initialState = {
+    loading : false,
+    error : '',
+    posts : []
+}
+
+const fetchPost = createAsyncThunk('post/fetchPost', async()=>{
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
+    const posts = await response.json();
+
+    return posts;
+})
+
+const postSlice = createSlice({
+    name : 'post',
+    initialState,
+    extraReducers : (builder) => {
+        builder.addCase(fetchPost.pending, (state, action)=>{
+            state.loading = true;
+            state.posts = [] ;
+            state.error = '';
+        });
+
+        builder.addCase(fetchPost.fulfilled, (state, action) => {
+            state.loading = false,
+            state.posts = action.payload
+        })
+        builder.addCase(fetchPost.rejected,(state, action)=>{
+            state.loading = false;
+            state.posts = [] ; 
+            state.error = action.error.message;
+        })
+    }
+})
+
+module.exports = postSlice.reducer;
+module.exports.fetchPost = fetchPost;
