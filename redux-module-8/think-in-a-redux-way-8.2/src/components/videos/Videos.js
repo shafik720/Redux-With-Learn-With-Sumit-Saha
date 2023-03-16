81ef3f9b5e269dc8ef0986fc9f74f92a43163f79
shafik720 <shafikrasel5@gmail.com>
2023-03-16 12:23:47 +0600

    videos fetched

--- redux-module-8/think-in-a-redux-way-8.2/src/components/videos/Videos.js
+++ redux-module-8/think-in-a-redux-way-8.2/src/components/videos/Videos.js
@@ -1,15 +1,25 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+import Error from "../ui/Error";
+import VideoLoader from "../ui/loaders/VideoLoader";
 import Video from "./Video";
 
 export default function Videos() {
+    const {data : videos, isLoading, isError} = useGetVideosQuery();
+    console.log(videos);
+
+    let content = null ;
+    if(isLoading && !isError){
+        content = <VideoLoader></VideoLoader>
+    }
+    if(!isLoading && isError){
+        content = <Error></Error>
+    }
+    if(!isLoading && !isError && videos.length > 0){
+        content = videos.map(video=><Video video={video} key={video.id}></Video>)
+    }
     return (
         <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
+            {content}
         </>
     );
 }
