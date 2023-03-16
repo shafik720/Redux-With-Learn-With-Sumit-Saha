bd4a962584675678b33c10b4928fbf1fd7229c60
shafik720 <shafikrasel5@gmail.com>
2023-03-16 19:21:26 +0600

    single video fetched done

--- redux-module-8/think-in-a-redux-way-8.2/src/components/pages/Video.js
+++ redux-module-8/think-in-a-redux-way-8.2/src/components/pages/Video.js
@@ -1,15 +1,37 @@
+import { useParams } from "react-router";
+import { useGetVideoQuery } from "../../features/api/apiSlice";
+import Error from "../ui/Error";
+import PlayerLoader from "../ui/loaders/PlayerLoader";
 import Description from "../video/Description";
 import Player from "../video/Player";
 import RelatedVideos from "../video/related/RelatedVideos";
 
 export default function Video() {
+    const { videoId } = useParams();
+    const { data: video, isLoading, isError } = useGetVideoQuery(videoId);
+
+    let content = null;
+    if (isLoading && !isError) {
+        content = <PlayerLoader></PlayerLoader>
+    }
+    if (!isLoading && isError) {
+        content = <Error message="some error happened"></Error>
+    }
+    if (!isLoading && !isError && !video.id) {
+        content = <Error message="No Videos found"></Error>
+    }
+    if (!isLoading && !isError && video.id) {
+        content = <>
+            <Player link={video.link} title={video.title} />
+            <Description video={video}/>
+        </>
+    }
     return (
         <section className="pt-6 pb-20 min-h-[calc(100vh_-_157px)]">
             <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
                 <div className="grid grid-cols-3 gap-2 lg:gap-8">
                     <div className="col-span-full w-full space-y-8 lg:col-span-2">
-                        <Player />
-                        <Description />
+                        {content}
                     </div>
 
                     <RelatedVideos />
--- redux-module-8/think-in-a-redux-way-8.2/src/components/videos/Video.js
+++ redux-module-8/think-in-a-redux-way-8.2/src/components/videos/Video.js
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import authorImage from "../../assets/author.png";
 
-export default function Video({video}) {
-    const {author, title, id, date, description, duration, link, thumbnail, views} = video;
+export default function Video({ video }) {
+    const { author, title, id, date, description, duration, link, thumbnail, views } = video;
     return (
         <div className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
             <div className="w-full flex flex-col">
@@ -28,13 +28,13 @@ export default function Video({video}) {
                     />
 
                     <div clas="flex flex-col">
-                        <Link to="/videos/1">
+                        <Link to={`/videos/${id}`}>
                             <p className="text-slate-900 text-sm font-semibold">
                                 {title}
                             </p>
                         </Link>
                         <span className="text-gray-400 text-xs hover:text-gray-600">
-                            {author} 
+                            {author}
                         </span>
                         <p className="text-gray-400 text-xs">
                             {views} views . {date}
