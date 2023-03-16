22c6c252739578ad330c921b2ab9c6e84d0e175e
shafik720 <shafikrasel5@gmail.com>
2023-03-16 12:30:09 +0600

    videos fetched

--- redux-module-8/think-in-a-redux-way-8.2/src/components/videos/Video.js
+++ redux-module-8/think-in-a-redux-way-8.2/src/components/videos/Video.js
@@ -1,21 +1,22 @@
 import { Link } from "react-router-dom";
 import authorImage from "../../assets/author.png";
 
-export default function Video() {
+export default function Video({video}) {
+    const {author, title, id, date, description, duration, link, thumbnail, views} = video;
     return (
         <div className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
             <div className="w-full flex flex-col">
                 <div className="relative">
-                    <Link to="/videos/1">
+                    <Link to={`/videos/${id}`}>
                         <img
-                            src="https://i3.ytimg.com/vi/6O4s7v28nlw/maxresdefault.jpg"
+                            src={thumbnail}
                             className="w-full h-auto"
                             alt="Some video title"
                         />
                     </Link>
 
                     <p className="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py">
-                        12:10
+                        {duration}
                     </p>
                 </div>
 
@@ -29,14 +30,14 @@ export default function Video() {
                     <div clas="flex flex-col">
                         <Link to="/videos/1">
                             <p className="text-slate-900 text-sm font-semibold">
-                                Video title
+                                {title}
                             </p>
                         </Link>
                         <span className="text-gray-400 text-xs hover:text-gray-600">
-                            Learn with Sumit
+                            {author} 
                         </span>
                         <p className="text-gray-400 text-xs">
-                            200 views . May 3, 2022
+                            {views} views . {date}
                         </p>
                     </div>
                 </div>
