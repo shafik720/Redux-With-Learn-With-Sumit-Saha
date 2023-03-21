fb49850382b77097e8f486bce2856196822677b6
shafik720 <shafikrasel5@gmail.com>
2023-03-21 22:36:46 +0600

    video 9.8 (login authentication checking)

--- redux-module-9/Chat App/src/App.js
+++ redux-module-9/Chat App/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useAuthCheck } from "./hooks/useAuthCheck";
 import Conversation from "./pages/Conversation";
 import Inbox from "./pages/Inbox";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
-    return (
+    const authCheck = useAuthCheck();
+
+    return !authCheck ? ( <div>Checking Authentication...</div> ) : (
         <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
